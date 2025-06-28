1c6fd035c7a36a60775277dda63828a7ef682492
Lody-dev <viaremko@c3r6s3.42malaga.com>
2025-06-28 16:21:16 +0200

    feat: fully integrated ft_strjoinv.c & ft_strlenv.c into libft.

--- libft/strings/ft_strlenv.c
+++ libft/strings/ft_strlenv.c
@@ -1,36 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoinv.c                                      :+:      :+:    :+:   */
+/*   ft_strlenv.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: viaremko <lodyiaremko@proton.me>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/28 15:31:38 by viaremko          #+#    #+#             */
-/*   Updated: 2025/06/28 16:11:32 by viaremko         ###   ########.fr       */
+/*   Created: 2025/06/28 15:41:14 by viaremko          #+#    #+#             */
+/*   Updated: 2025/06/28 16:09:15 by viaremko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../libft.h"
 
-char	*strjoinv(const char *str, ...)
+size_t	ft_strlenv(va_list args)
 {
-	char	*return_str;
-	char	*tmp_str;
 	size_t	size;
-	va_list	args;
-	va_list	tmp_args;
+	char	*tmp_str;
 
-	va_start(args, str);
-	va_copy(tmp_args, args);
-	size = ft_strlen(str) + ft_strlenv(tmp_args) + 1;
-	return_str = ft_calloc(size, sizeof(char));
-	ft_strlcpy(return_str, str, size);
+	size = 0;
 	tmp_str = va_arg(args, char *);
 	while (tmp_str)
 	{
-		ft_strlcat(return_str, tmp_str, size);
+		size += ft_strlen(tmp_str);
 		tmp_str = va_arg(args, char *);
 	}
-	va_end(args);
-	va_end(tmp_args);
-	return (return_str);
+	return (size);
 }
