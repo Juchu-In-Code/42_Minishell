75e6699d1ca21a89c5d2b88c3bd0c8cf02bbd9b7
Lody-dev <viaremko@c3r6s3.42malaga.com>
2025-06-28 16:49:19 +0200

    refactor: ft_strlenv.c must receive a copy of va_list args not the original pointer

--- libft/strings/ft_strlenv.c
+++ libft/strings/ft_strlenv.c
@@ -6,22 +6,22 @@
 /*   By: viaremko <lodyiaremko@proton.me>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 15:41:14 by viaremko          #+#    #+#             */
-/*   Updated: 2025/06/28 16:09:15 by viaremko         ###   ########.fr       */
+/*   Updated: 2025/06/28 16:43:26 by viaremko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../libft.h"
 
-size_t	ft_strlenv(va_list args)
+size_t	ft_strlenv(va_list args_copy)
 {
 	size_t	size;
 	char	*tmp_str;
 
 	size = 0;
-	tmp_str = va_arg(args, char *);
+	tmp_str = va_arg(args_copy, char *);
 	while (tmp_str)
 	{
 		size += ft_strlen(tmp_str);
-		tmp_str = va_arg(args, char *);
+		tmp_str = va_arg(args_copy, char *);
 	}
 	return (size);
 }
