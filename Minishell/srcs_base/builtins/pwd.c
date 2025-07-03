65fa11eb0e8faaf55caa4f92ca3aca6b7b48ffd2
Lody-dev <viaremko@c3r6s3.42malaga.com>
2025-07-03 14:36:48 +0200

    refactor: norminette friendly

--- Minishell/srcs_base/builtins/pwd.c
+++ Minishell/srcs_base/builtins/pwd.c
@@ -6,14 +6,15 @@
 /*   By: viaremko <lodyiaremko@proton.me>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 11:05:03 by viaremko          #+#    #+#             */
-/*   Updated: 2025/06/30 11:12:22 by viaremko         ###   ########.fr       */
+/*   Updated: 2025/07/03 14:36:00 by viaremko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../z_minishell.h"
 
-void pwd()
+void	pwd(void)
 {
-	char *cwd;
+	char	*cwd;
+
 	cwd = malloc(PATH_MAX);
 	getcwd(cwd, PATH_MAX);
 	printf("%s\n", cwd);
