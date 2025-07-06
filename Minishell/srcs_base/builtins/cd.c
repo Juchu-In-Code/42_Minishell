2ccc1a29e8fc0dd80d05edc23cdc61e0db5713f4
Lody-dev <lodyiaremko@proton.me>
2025-07-06 13:59:21 +0200

    refactor: cleaner cd

--- Minishell/srcs_base/builtins/cd.c
+++ Minishell/srcs_base/builtins/cd.c
@@ -13,25 +13,18 @@
 
 void    cd(t_shell *shell, int ac, char **av)
 {
-        t_env *env_node;
-        char *dir_path;
-        char *tmp;
-
-        if(ac > 2)
-                printf("minishell: cd: too many arguments\n");
-        if(av[1])
-                dir_path =ft_strdup(av[1]);
-        else
-                dir_path = ft_strdup(get_env_val(shell->env, "HOME"));
-        if(chdir(dir_path) != 0) //0 is success in case of chdir func
-                return((void)printf("Cant find path\n"));
-        tmp = ft_strdup(get_env_val(shell->env, "PWD"));
-        env_node = get_env(shell->env, "PWD");
-        change_env_value(env_node, dir_path);
-        char *debug = get_env_val(shell->env, "PWD");
-        printf("%s\n", debug);
-        env_node = get_env(shell->env, "OLDPWD");
-        change_env_value(env_node, tmp);
-        free(dir_path);
-        free(tmp);
+        char *dir_path;                                        
+        char *tmp;                                                                                                         
+                                                                                                                           
+        if(ac > 2)                                                                                                         
+                return((void)printf("minishell: cd: too many arguments\n"));                                               
+        if(av[1])                                                                                                          
+                dir_path = av[1];                                                                                          
+        else                                                                                                               
+                dir_path = get_env_val(shell->env, "HOME");
+        if(chdir(dir_path) != 0)
+                return((void)printf("minishell: cd: can't find path\n"));
+        tmp = get_env_val(shell->env, "PWD");
+        change_env_value(get_env(shell->env, "PWD"), dir_path);
+        change_env_value(get_env(shell->env, "OLDPWD"), tmp);
 }
