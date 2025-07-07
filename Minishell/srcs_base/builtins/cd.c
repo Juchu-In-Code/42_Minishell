81bb2955f2b9e8ccc569e6d2e5adfd007a67d2aa
Lody-dev <lodyiaremko@proton.me>
2025-07-07 19:44:01 +0200

    feat: add support for 'cd -' and refactor path resolution

--- Minishell/srcs_base/builtins/cd.c
+++ Minishell/srcs_base/builtins/cd.c
@@ -11,29 +11,41 @@
 /* ************************************************************************** */
 #include "../z_minishell.h"
 
+static char *resolve_path(char **av, t_shell *shell)
+{
+	char *tmp;
+
+	if(av[1] && av[1][0] == '/') //in case of absolute path;
+		return (ft_strdup(av[1]));
+	if(av[1] && av[1][0] == '-' && av[1][1] == '\0')
+	{
+        	if(!get_env(shell->env, "OLDPWD"))
+			return (printf(":( Can't find  OLDPWD\n"), NULL);
+		tmp = ft_strdup(get_env_val(shell->env, "OLDPWD"));
+		printf("%s\n", tmp);
+		return (tmp);
+	}
+	if(av[1])
+	{
+		tmp = get_env_val(shell->env, "PWD");
+		tmp = ft_strjoinv(tmp,"/",av[1],NULL); 
+		if(!tmp)
+			return (printf("Bad join\n"), NULL);
+		return(tmp);
+	}
+	return(ft_strdup(get_env_val(shell->env, "HOME")));
+}
+
 void    cd(t_shell *shell, int ac, char **av)
 {
         char *dir_path;                                        
         char *tmp;                                                                                                         
                                                                                                                            
-        if(ac > 2)                                                                                                         
-                return((void)printf("minishell: cd: too many arguments\n"));                                               
-        if(av[1])                                                                                                          
-        {
-                if(ft_strncmp(av[1],"/",1) == 0) //in case of absolute path;
-                        dir_path = ft_strdup(av[1]);                                                                                     
-                else
-                {
-                        tmp = get_env_val(shell->env, "PWD");
-                        dir_path = ft_strjoinv(tmp,"/",av[1],NULL); 
-			if(!dir_path)
-                		return((void)printf("minishell: cd: bad malloc\n"));
-                }
-        }
-        else                                                                                                               
-                dir_path = ft_strdup(get_env_val(shell->env, "HOME"));
-        if(chdir(dir_path) != 0)
-                return((void)printf("minishell: cd: can't find path\n"));
+	if(ac > 2)                                                                                                         
+		return((void)printf("minishell: cd: too many arguments\n"));                                               
+	dir_path = resolve_path(av, shell);
+	if(chdir(dir_path) != 0)
+                return((void)perror("minishell: cd:"));
         if(!get_env(shell->env, "OLDPWD"))
                 list_insert_tail(shell->env,create_dict_entry(ft_strdup("OLDPWD"),ft_strdup(""), 0));
         tmp = ft_strdup(get_env_val(shell->env, "PWD"));
