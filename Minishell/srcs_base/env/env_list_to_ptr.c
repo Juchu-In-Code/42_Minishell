7960c869c603b106cd0d346ae79aa8548fd1f7d5
Lody-dev <lodyiaremko@proton.me>
2025-07-17 18:57:38 +0200

    refactor: norminette frinedly

--- Minishell/srcs_base/env/env_list_to_ptr.c
+++ Minishell/srcs_base/env/env_list_to_ptr.c
@@ -13,54 +13,54 @@
 
 static char	**alloc_env_array(size_t size)
 {
-        char **result;
+	char	**result;
 
-        result = malloc(sizeof(char *) * (size + 1));
-        if (result)
-                result[size] = NULL;
-        return (result);
+	result = malloc(sizeof(char *) * (size + 1));
+	if (result)
+		result[size] = NULL;
+	return (result);
 }
 
-static char *make_env_entry(t_env *env)
+static	char	*make_env_entry(t_env *env)
 {
-    if (!env || !env->dict[KEY] || !env->dict[VAL])
-        return (NULL);
-    return (strjoinv(env->dict[KEY], "=", env->dict[VAL], NULL));
+	if (!env || !env->dict[KEY] || !env->dict[VAL])
+		return (NULL);
+	return (strjoinv(env->dict[KEY], "=", env->dict[VAL], NULL));
 }
 
 static bool	fill_env_array(char **result, t_list *env)
 {
-        t_item  *curr;
-        t_env   *curr_env;
-        int     i;
+	t_item	*curr;
+	t_env	*curr_env;
+	int		i;
 
-        i = 0;
-        curr = env->head;
-        while (curr)
-        {
-                curr_env = (t_env *)curr->data;
-                result[i] = make_env_entry(curr_env);
-                if (!result[i])
-                        return (false);
-                i++;
-                curr = curr->next;
-        }
-        return (true);
+	i = 0;
+	curr = env->head;
+	while (curr)
+	{
+		curr_env = (t_env *)curr->data;
+		result[i] = make_env_entry(curr_env);
+		if (!result[i])
+			return (false);
+		i++;
+		curr = curr->next;
+	}
+	return (true);
 }
 
 char	**env_list_to_ptr(t_list *env)
 {
-        char    **result;
+	char	**result;
 
-        if (!env || env->size == 0)
-                return (NULL);
-        result = alloc_env_array(env->size);
-        if (!result)
-                return (NULL);
-        if (!fill_env_array(result, env))
-        {
-                ft_free_matrix((void **)result);
-                return (NULL);
-        }
-        return (result);
+	if (!env || env->size == 0)
+		return (NULL);
+	result = alloc_env_array(env->size);
+	if (!result)
+		return (NULL);
+	if (!fill_env_array(result, env))
+	{
+		ft_free_matrix((void **)result);
+		return (NULL);
+	}
+	return (result);
 }
