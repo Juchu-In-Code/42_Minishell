bf8ae11e51907b6f06d5f8baee5fc6a3641c717a
Julian Galizio <jgalizio@c3r8s4.42malaga.com>
2026-06-25 10:35:07 +0200

    error return

--- Minishell/libft/error/libft_error.h
+++ Minishell/libft/error/libft_error.h
@@ -6,7 +6,7 @@
 /*   By: jgalizio <jgalizio@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 12:53:16 by jgalizio          #+#    #+#             */
-/*   Updated: 2025/06/06 15:33:24 by jgalizio         ###   ########.fr       */
+/*   Updated: 2026/06/25 10:34:19 by jgalizio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ void	error_call(char *err, char *msg);
 void	error_exit(char *err, char *msg, int exit_code);
 void	perror_call(char *err, char *msg);
 void	perror_exit(char *err, char *msg, int exit_code);
+int	error_return(char *err, char *msg, int return_code);
 
 #endif
