5b1aa5b160f33fbe846d715dc89b97e35d9d2a09
Lody-dev <lodyiaremko@proton.me>
2026-06-25 17:31:23 +0200

    norm things

--- Minishell/srcs_base/expansion/expansor.c
+++ Minishell/srcs_base/expansion/expansor.c
@@ -11,22 +11,20 @@
 /* ************************************************************************** */
 #include "../z_minishell.h"
 
-static char *append_string(char *s1, char *s2)
+static char	*append_string(char *s1, char *s2)
 {
 	char	*new_str;
-    
+
 	if (!s1)
 		s1 = ft_strdup("");
 	if (!s2)
 		return (s1);
-
 	new_str = ft_strjoin(s1, s2);
-	//free(s1);
-	ft_free((void**)&s1);
+	ft_free((void **)&s1);
 	return (new_str);
 }
 
-static char *append_char(char *s, char c)
+static char	*append_char(char *s, char c)
 {
 	char	str[2];
 
@@ -35,79 +33,51 @@ static char *append_char(char *s, char c)
 	return (append_string(s, str));
 }
 
-static char *process_dollar(char *str, int *i, t_shell *shell)
+static char	*process_dollar(char *str, int *i, t_shell *shell)
 {
-	int	start;
-	int	len;
+	int		start;
+	int		len;
 	char	*key;
 	char	*val;
 
-	//start will contain a char after $
 	start = *i + 1;
 	len = 0;
-
-	//expands $?
 	if (str[start] == '?')
-	{
 		*i += 1;
+	if (str[start] == '?')
 		return (ft_itoa(shell->last_exit_code));
-	}
-
-	//count the lenth of env variable to expand
-	while (str[start + len])
-	{
-		if (!ft_isalnum(str[start + len]) && str[start + len] != '_')
-			break;
+	while (str[start + len] && (ft_isalnum(str[start + len]) || str[start
+				+ len] == '_'))
 		len++;
-	}
-
-	//only $ 
 	if (len == 0)
 		return (ft_strdup("$"));
-
-	//substring the word after $
 	key = ft_substr(str, start, len);
-
-	//skip processed word
 	*i += len;
-
-	//get value
 	val = get_env_val(shell->env, key);
-
-	//free(key);
-	ft_free((void**)&key);
-
-	//to not acciedently get rid of original pointer
+	ft_free((void **)&key);
 	if (val)
 		return (ft_strdup(val));
-    
-	//no value for a key
 	return (ft_strdup(""));
 }
 
-char *expand(char *str, t_shell *shell)
+char	*expand(char *str, t_shell *shell)
 {
 	char	*res;
 	char	*tmp_val;
-	int	i;
+	int		i;
 
 	if (!str)
 		return (NULL);
-
-	//str for the result of joins
 	res = ft_strdup("");
 	i = 0;
-
 	while (str[i])
 	{
-		//caso de $
 		if (str[i] == '$')
 		{
 			tmp_val = process_dollar(str, &i, shell);
 			res = append_string(res, tmp_val);
-			//free(tmp_val);
-			ft_free((void**)&tmp_val);
-		}//cualquier otro caracter
+			ft_free((void **)&tmp_val);
+		}
 		else
 			res = append_char(res, str[i]);
 		i++;
--- Minishell/srcs_base/redirections/heredoc.c
+++ Minishell/srcs_base/redirections/heredoc.c
@@ -11,9 +11,9 @@
 /* ************************************************************************** */
 #include "../z_minishell.h"
 
-//this function generates a name for each heredoc.
-//i is static to maintain the heredoc file number;
-static char *get_temp_name()
+// this function generates a name for each heredoc.
+// i is static to maintain the heredoc file number;
+static char	*get_temp_name(void)
 {
 	static int	i;
 	char		*name;
@@ -21,48 +21,48 @@ static char *get_temp_name()
 
 	num = ft_itoa(i++);
 	name = ft_strjoin("/tmp/.minishell_heredoc_", num);
-	//free(num);
-	ft_free((void**)&num);
+	ft_free((void **)&num);
 	return (name);
 }
 
-// this function handles the heredoc and converts it into a simple imput redirection;
-// function returns a filename to store into cmd struct and mark it as INPUT redirection;
-char	*process_heredoc(char *delimiter, bool has_quotes, t_shell *shell)
+static void	write_heredoc(int fd, char *delimiter, bool quotes, t_shell *shell)
 {
-	char	*filename;
 	char	*input;
 	char	*to_write;
-	int	fd;
 
-	filename = get_temp_name();
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0644);
-	if(fd < 0)
-	{
-		ft_free((void**)&filename);
-		return(NULL);
-	}
-	while(1337)
+	while (1337)
 	{
 		input = readline("> ");
-		if(!input || ft_strcmp(input, delimiter) == 0)
+		if (!input || ft_strcmp(input, delimiter) == 0)
 		{
-			if(input)
-				ft_free((void**)&input);
-				//free(input);
-			break;
+			if (input)
+				ft_free((void **)&input);
+			break ;
 		}
-		if(has_quotes == true)
+		if (quotes == true)
 			to_write = ft_strdup(input);
 		else
 			to_write = expand(input, shell);
-
 		write(fd, to_write, ft_strlen(to_write));
 		write(fd, "\n", 1);
-
-		ft_free((void**)&input);
-		ft_free((void**)&to_write);
+		ft_free((void **)&input);
+		ft_free((void **)&to_write);
 	}
+}
+
+char	*process_heredoc(char *delimiter, bool has_quotes, t_shell *shell)
+{
+	char	*filename;
+	int		fd;
+
+	filename = get_temp_name();
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (fd < 0)
+	{
+		ft_free((void **)&filename);
+		return (NULL);
+	}
+	write_heredoc(fd, delimiter, has_quotes, shell);
 	close(fd);
 	return (filename);
 }
